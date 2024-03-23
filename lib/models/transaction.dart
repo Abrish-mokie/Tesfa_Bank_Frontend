3c79e547d3e971bdabdb4342e75aae9f526b51a3
Berhan-Adhana <berhanghe90@gmail.com>
2024-03-23 01:01:46 -0600

    image and isWithdrawal fields added

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -2,10 +2,13 @@ class Transaction {
   final String name;
   final String date;
   final String amount;
+  final String imageUrl;
+  final bool isWithdraw;
 
-  Transaction({
-    required this.name,
-    required this.date,
-    required this.amount,
-  });
+  Transaction(
+      {required this.name,
+      required this.date,
+      required this.amount,
+      required this.imageUrl,
+      required this.isWithdraw});
 }
